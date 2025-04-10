a80ac386a3c990e8211f272a71e1827b71fe0ef9
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-04-11 01:39:24 +0200

    Include tuple backport and remove MonoMod.Backports dependency (#1)
    * Include tuple backport and remove MonoMod.Backports dep
    
    * Make it more like in monomod
    
    * Add warning disable

---
+++ Backport/ITuple.cs
@@ -0,0 +1,21 @@
+﻿#if NET35
+
+#pragma warning disable CS8632 // The annotation for nullable reference types should only be used in code within a '#nullable' annotations context.
+
+namespace System.Runtime.CompilerServices {
+    /// <summary>
+    /// This interface is required for types that want to be indexed into by dynamic patterns.
+    /// </summary>
+    internal interface ITuple {
+        /// <summary>
+        /// The number of positions in this data structure.
+        /// </summary>
+        int Length { get; }
+
+        /// <summary>
+        /// Get the element at position <param name="index"/>.
+        /// </summary>
+        object? this[int index] { get; }
+    }
+}
+#endif
