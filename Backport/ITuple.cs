ceb207b4f66d97009dcf560ae84692069edd4ef9
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-04-11 00:22:52 +0200

    Include tuple backport and remove MonoMod.Backports dep

---
+++ Backport/ITuple.cs
@@ -0,0 +1,18 @@
+﻿#if NET35
+namespace System.Runtime.CompilerServices;
+
+/// <summary>
+/// This interface is required for types that want to be indexed into by dynamic patterns.
+/// </summary>
+internal interface ITuple {
+    /// <summary>
+    /// The number of positions in this data structure.
+    /// </summary>
+    int Length { get; }
+
+    /// <summary>
+    /// Get the element at position <param name="index"/>.
+    /// </summary>
+    object? this[int index] { get; }
+}
+#endif
